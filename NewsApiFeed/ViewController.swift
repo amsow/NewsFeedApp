810d2cb5c279cc94ae97c15ea8ca87460adbd07d
ads <amadou.s@makeba.money>
2021-10-22 12:45:17 +0000

    Initial Commit

---
+++ NewsApiFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApiFeed
+//
+//  Created by Amadou Diarra SOW on 22/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
