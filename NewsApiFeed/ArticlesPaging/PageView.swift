d713b8dba11bfdcf1fd3f5817a97f3f2495872d0
ads <amadou.s@makeba.money>
2021-11-14 13:16:57 +0000

    Setup ArticlesPageViewController with `UIViewControllerRepresentable` and embedded `Coordinator` for SwiftUI implementation sake

--- NewsApiFeed/ArticlesPaging/PageView.swift
+++ NewsApiFeed/ArticlesPaging/PageView.swift
@@ -8,31 +8,76 @@
 import SwiftUI
 
 @available(iOS 13, *)
-struct ArticlesPageView: View {
+struct ArticlesPageView<ArticleDetailView: HostableView>: View {
+    
+    let pageViews: [ArticleDetailView]
+    
     var body: some View {
-        Text("Hello World!")
+        ArticlesPageViewController(pages: pageViews)
     }
 }
 
 @available(iOS 13, *)
 struct ArticlesPageView_Previews: PreviewProvider {
     static var previews: some View {
-        ArticlesPageView()
+        ArticlesPageView(pageViews: [ArticleDetailView(article: Article.fake), ArticleDetailView(article: Article.fake), ArticleDetailView(article: Article.fake)])
     }
 }
 
 
 @available(iOS 13, *)
-struct ArticlesPageViewController<PageView: View>: UIViewControllerRepresentable {
+struct ArticlesPageViewController<PageView: HostableView>: UIViewControllerRepresentable {
     
-    let pageView: [PageView]
+    let pages: [PageView]
     
-    func makeUIViewController(context: Context) -> some UIPageViewController {
-        let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
         return pageViewController
     }
     
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    func updateUIViewController(_ uiViewController: UIPageViewController, context: Context) {
+        guard let firstController = context.coordinator.controllers.first else { return }
+        uiViewController.setViewControllers([firstController], direction: .forward, animated: true)
+    }
+}
+
+@available(iOS 13, *)
+extension ArticlesPageViewController {
+    
+    // MARK: - Coordinator - DataSource
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        let parent: ArticlesPageViewController
+        var controllers = [UIViewController]()
         
+        init(_ pageViewController: ArticlesPageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { $0.viewController }
+        }
+        
+        // MARK: - UIPageViewControllerDataSource
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController), index > 0 else {
+                return nil
+            }
+            let previousIndex = index - 1
+            let vc = controllers[previousIndex]
+           // navigationTitle = viewController.navigationItem.title
+            return vc
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController), index < (controllers.count - 1) else {
+                return nil
+            }
+            let nextIndex = index + 1
+            let vc = controllers[nextIndex]
+          //  navigationTitle = viewController.navigationItem.title
+            return vc
+        }
     }
 }
